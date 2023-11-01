e88554c8f81c09cd8c45747e601f1eead788ded2
asanche4 <asanche4@uccs.edu>
2023-11-01 15:52:26 -0600

    list and detail HTML

--- nurse_app/views.py
+++ nurse_app/views.py
@@ -21,12 +21,18 @@ def index(request):
 #these classes contain the templates of how are going to be display and getting the queries fro the database.
 class ScrubsListView(generic.ListView):
    model = Scrubs
+   
+   def scrub_list_view(request):
+        scrubs_list = Scrubs.objects.all()  # Fetch all Scrub objects from the database
+        return render(request, 'nurse_app/scrubs_list.html', {'scrub_list': scrubs_list})
 class ScrubsDetailView(generic.DetailView):
    model = Scrubs
 
+   def scrub_detail(self, request, pk):
+        scrubs = Scrubs.objects.get(pk=pk)  # Retrieve the specific Scrub object
+        return render(request, 'scrubs_detail.html', {'scrub': scrubs})
 
 
-def scrub_list_view(request):
-    scrub_list = Scrubs.objects.all()  # Fetch all Scrub objects from the database
-    return render(request, 'nurse_app/scrubs_list.html', {'scrub_list': scrub_list})
+
+
 
